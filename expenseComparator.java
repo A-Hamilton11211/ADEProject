f9c0300c18bdac72510d35acd1350ccfcbe0b0df
Angus Hamilton <ahamilton11211@gmail.com>
2017-02-20 15:26:23 +0000

    expenseComparator v1.1
    -Small change to expenseComparator to use the correct getCost() method instead of getExp(), which is not a method, in journey.java

--- expenseComparator.java
+++ expenseComparator.java
@@ -1,8 +1,8 @@
 import java.util.Comparator;
 public class expenseComparator implements Comparator<Journey> {
 	public int compare(Journey s1, Journey s2){
-		double t1 = s1.getExp();
-		double t2 = s2.getExp();
+		double t1 = s1.getCost();
+		double t2 = s2.getCost();
 		if (t1<t2)return 1;
 		else if (t1>t2) return -1;
 		else return 0;
