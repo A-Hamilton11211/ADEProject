90c4e8691ebcd4ee2975d46281ef23db0b07fb3d
Angus Hamilton <ahamilton11211@gmail.com>
2017-02-21 18:40:35 +0000

    Delete ImpossibleDistException.java

--- main/ImpossibleDistException.java
+++
@@ -1,9 +0,0 @@
-package main;
-@SuppressWarnings("serial")
-public class ImpossibleDistException extends Exception {
-
-	// This class implements an error that triggers when the distance of any journey is 0 or below, which is impossible
-	public ImpossibleDistException(double invalidDist){
-		super("Cannot have a journey equal to or below 0 miles");
-	}
-}
