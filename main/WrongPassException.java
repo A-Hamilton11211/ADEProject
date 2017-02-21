02d2be8a24791b7be19fcbc38a7233549fa91f05
Angus Hamilton <ahamilton11211@gmail.com>
2017-02-21 19:35:12 +0000

    Delete WrongPassException.java

--- main/WrongPassException.java
+++
@@ -1,8 +0,0 @@
-package main;
-@SuppressWarnings("serial")
-public class WrongPassException extends Exception {
-	//This class implements an exception that limits the number of passengers to be between 1 to 6 within the journey class
-	public WrongPassException(int badPass){
-		super(badPass + " is either too many, or too few passengers");
-	}
-}
